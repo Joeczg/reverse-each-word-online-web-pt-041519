1c540eb5452c6e65986286b8e5298bfc3e72a8a2
zhenggao cai <caizhengao1992@gmail.com>
2019-04-19 00:52:40 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,5 +1,7 @@
 def reverse_each_word(statement)
   new_array=statement.split(" ")
-  new_array.collect do |name| name.reverse end
+  reverse_array=new_array.collect { |name| name.reverse }
+  reverse_array.join(" ")
+  
 end
   
